062bccdbd075435b1a7b54fbe964c62e3a987617
jack-ie <jackie919@outlook.com>
2022-01-26 14:39:53 -0600

    deleted:    HW Resources/HWREADME.md 	deleted:    HW Resources/script2.js 	modified:   README.md 	modified:   develop/index.html 	modified:   develop/script.js

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# 3-passcode-generator
+# Passcode Generator
+
+### Generate a random passcode for any personal use to have security over any of your accounts.
+
+---
+
+## Deployed Website
+Screenshot taken from desktop computer:
+
+![Landing Page](assets\03-javascript-homework-demo.png)
+
+Link to the deployed page:
+
+[Passcode Generator](https://jack-ie.github.io/3-passcode-generator/)
+
+---
+
+## Usage
+User clicks "Generate Passcode" and answers the following prompts:
+* How many characters for your passcode?
+* Would you like UPPERCASE letters?
+* Would you like lowercase letters?
+* Would you like numbers?
+* Would you like special characters?
+
+ For any of these prompts, "OK" registers as "yes" and "Cancel" as "No".
+
+ A randomized input will appear. You can now copy and paste your new random passcode.
+
+ ---
+
+ ## Future Development
+ #### Will implement checkboxes to apply options for passcode instead of prompts.
+ ---
+
+ ### Author
+ Jackeline Barrios
+
+ Github: [Github Link](https://github.com/jack-ie)
--- develop/script.js
+++ develop/script.js
@@ -7,80 +7,80 @@ var choices;
 var passcode;
 
 //Character references
-character = ["!","#","$","%","&","'","(", ")","*","+",",","-",".","/",":",";","<","?","@","[","^","]","_","`","{","}","|","~"];
-numeric = ["1","2","3","4","5","6","7","8","9","0"];
-upper = ["Q","W","E","R","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
-lower =["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+character = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ',', '.', '/', '?', '>', '<'];
+numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+upper = ["Q", "W", "E", "R", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+lower = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", function () {
-   passcode = writePasscode();
-   document.getElementById("password").placeholder = passcode;
+  passcode = newPasscode();
+  document.getElementById("password").placeholder = passcode;
 });
 
-// Write password to the #password input
-function writePasscode() {
-    var characterAmount = window.prompt("How many characters long would you like your passcode to be? (Between 8 and 128)");
-    if (!characterAmount) {
-        alert("You need to enter a value");
-    } else if (characterAmount < 8 || characterAmount > 128) {
-        window.prompt("Please enter a number between 8 and 128.");
-    writePasscode();
-    } else {
-        specialCharacters = confirm("Would you like special characters?");
-        lowerCase = confirm("Would you like lowercase letters?");
-        upperCase = confirm("Would you like uppercase letters?");
-        numericCharacters = confirm("Would you like numbers?");
-    };
+//password to the #password input
+function newPasscode() {
+  var characterAmount = window.prompt("How many characters for your passcode? (Between 8 and 128)");
+  if (!characterAmount) {
+    alert("You need to enter a value");
+  } else if (characterAmount < 8 || characterAmount > 128) {
+    window.prompt("Please enter a number between 8 and 128");
+    newPasscode();
+  } else {
+    upperCase = confirm("Would you like UPPERCASE letters?");
+    lowerCase = confirm("Would you like lowercase letters?");
+    numericCharacters = confirm("Would you like numbers?");
+    specialCharacters = confirm("Would you like special characters?");
+  };
 
-    //no options choosen
-    if (!specialCharacters && !lowerCase && !upperCase && !numericCharacters) {
-        choices = alert("You must choose a criteria.");
-    writePasscode();
-    }
+  //no options choosen
+  if (!specialCharacters && !lowerCase && !upperCase && !numericCharacters) {
+    choices = alert("You must choose a criteria");
+    newPasscode();
+  }
 
-    //all options choosen
-    else if (specialCharacters &&  numericCharacters && upperCase && lowerCase) {
-        choices = character.concat(numeric, lower, upper);
-    }
+  //all options choosen
+  else if (specialCharacters && numericCharacters && upperCase && lowerCase) {
+    choices = character.concat(numeric, lower, upper);
+  }
 
-    //3 options choosen
-    else if (specialCharacters && numericCharacters && upperCase) {
-        choices = character.concat(numeric, upper);
-    } else if (specialCharacters && numericCharacters && lowerCase) {
-        choices = character.concat (number, lower);
-    } else if (specialCharacters && lowerCase && upperCase) {
-        choices = character.concat(lower, upper);
-    } else if (numericCharacters && lowerCase && upperCase) {
-        choices = number.concat(lower, upper);
-    }
+  //3 options choosen
+  else if (specialCharacters && numericCharacters && upperCase) {
+    choices = character.concat(numeric, upper);
+  } else if (specialCharacters && numericCharacters && lowerCase) {
+    choices = character.concat(number, lower);
+  } else if (specialCharacters && lowerCase && upperCase) {
+    choices = character.concat(lower, upper);
+  } else if (numericCharacters && lowerCase && upperCase) {
+    choices = number.concat(lower, upper);
+  }
 
   //2 options choosen
-    else if (specialCharacters && numericCharacters) {
-        choices = character.concat(numeric);
-    } else if (specialCharacters && lowerCase) {
-        choices = character.concat(lower);
-    } else if (specialCharacters && upperCase) {
-        choices = character.concat(upper);
-    } else if (lowerCase && numericCharacters) {
-        choices = lower.concat(numeric);
-    } else if (lowerCase && upperCase) {
-        choices = lower.concat(upper);
-    } else if (numericCharacters && upperCase) {
-        choices = numeric.concat(upper);
-    }
+  else if (specialCharacters && numericCharacters) {
+    choices = character.concat(numeric);
+  } else if (specialCharacters && lowerCase) {
+    choices = character.concat(lower);
+  } else if (specialCharacters && upperCase) {
+    choices = character.concat(upper);
+  } else if (lowerCase && numericCharacters) {
+    choices = lower.concat(numeric);
+  } else if (lowerCase && upperCase) {
+    choices = lower.concat(upper);
+  } else if (numericCharacters && upperCase) {
+    choices = numeric.concat(upper);
+  }
 
   //1 option
-    else if (specialCharacters) {
-        choices = character;
-    } else if (numericCharacters) {
-        choices = numeric;
-    } else if (lowerCase) {
-        choices = lower;
-    } else if (upperCase) {
-        choices = upper;
-    };
+  else if (specialCharacters) {
+    choices = character;
+  } else if (numericCharacters) {
+    choices = numeric;
+  } else if (lowerCase) {
+    choices = lower;
+  } else if (upperCase) {
+    choices = upper;
+  };
 
   //array placeholder
   var password = [];
@@ -89,13 +89,15 @@ function writePasscode() {
   for (var i = 0; i < characterAmount; i++) {
     var pickChoices = choices[Math.floor(Math.random() * choices.length)];
     password.push(pickChoices);
+  }
+
+  //turns array into string 
+  var passcode = password.join("");
+  UserInput(passcode);
+  return passcode;
 }
-    //turns array into string 
-    var passcode = password.join("");
-    UserInput(passcode);
-    return passcode;
-}
+
 //Puts passcode in alert
-    function UserInput(passcode) {
-    document.getElementById("password").textContent = passcode;
+function UserInput(passcode) {
+  document.getElementById("password").textContent = passcode;
 }
